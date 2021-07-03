619c4e587b8d99127b35ddc9da9d6db440610077
LeandroParisi <lepcbelisario@gmail.com>
2021-07-03 10:33:06 -0300

    setup: creates base app structure

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { ToastContainer } from 'react-toastify'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { RootProvider } from './store'
+import { Login } from './pages'
+
+function App() {
+  return (
+    <>
+      <ToastContainer />
+      <BrowserRouter>
+        <RootProvider>
+          <Switch>
+            <Route exact path="/app/login" component={Login} />
+          </Switch>
+        </RootProvider>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import { resets } from 'assets/scss/reset.scss.d.ts'
+import classNames from 'classnames'
+import styles from './Login.module.scss'
+
+const Login = () => (
+  <div className={classNames(styles.loginPage)}>
+    Login
+  </div>
+)
+
+export default Login
