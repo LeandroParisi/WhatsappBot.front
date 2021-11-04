9c434cef6da418e81e6d79105ba6b39ec794ceec
LeandroParisi <lepcbelisario@gmail.com>
2021-11-03 21:28:32 -0300

    refactor: adds template folder to appCustomer folders

---
+++ src/appCustomer/components/_TEMPLATE/SideBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const SideBar = () => {
+  const nothing = 0
+  return (
+    <div>
+      SideBar
+    </div>
+  )
+}
+
+export default SideBar
